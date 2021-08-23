fbf0eb1f918c871393fe45251e18de820ea7876d
royel21 <royelconsoro@hotmail.com>
2021-08-22 22:15:48 -0400

    remove Serialize Code

--- Plutus/src/ECS/Components/Velocity.h
+++ Plutus/src/ECS/Components/Velocity.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "Serialize/Serialize.h"
 
 namespace Plutus
 {
@@ -9,17 +8,4 @@ namespace Plutus
         float y;
         Velocity(float _x, float _y) : x(_x), y(_y) {};
     };
-
-    inline void Velocity_JSON(Serializer& serializer, const Velocity& v) {
-        auto writer = serializer.getWriter();
-        writer->StartObject();
-        {
-            writer->String("name");
-            writer->String("Velocity");
-            writer->String("x");
-            writer->Double(v.x);
-            writer->String("y");
-            writer->Double(v.y);
-        }
-    }
 } // namespace Plutus
