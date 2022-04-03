dc7394608701904be47fec8bbe301449014735e9
royel <royelconsoro@hotmail.com>
2022-04-03 18:39:48 -0400

    Reorder project for web dev and remove duplicate folder

--- web/assets/tilemaps/Jungle_Terraint.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Jungle_Terraint" tilewidth="32" tileheight="32" tilecount="420" columns="28">
- <image source="Jungle_terrain.png" width="896" height="480"/>
-</tileset>
