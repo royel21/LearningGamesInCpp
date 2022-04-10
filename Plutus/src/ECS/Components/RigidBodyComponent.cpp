9689080548e9ca623d51921bddb59d9036aa6413
royel <royelconsoro@hotmail.com>
2022-04-10 12:26:11 -0400

    add max velocity facilities

--- Plutus/src/ECS/Components/RigidBodyComponent.cpp
+++ Plutus/src/ECS/Components/RigidBodyComponent.cpp
@@ -29,4 +29,37 @@ namespace Plutus
     void RigidBodyComponent::setVelocity(float x, float y) {
         mBody->SetLinearVelocity(b2Vec2(x, y));
     }
+
+    vec2f RigidBodyComponent::getVelocity() {
+        auto vel = mBody->GetLinearVelocity();
+        return { vel.x , vel.y };
+    }
+
+    void RigidBodyComponent::speedLimit() {
+        float x = mMaxVel.x, y = mMaxVel.y;
+        if (x > 0 || y > 0) {
+            auto vel = getVelocity();
+            if (x) {
+                if (x > 0) {
+                    if (vel.x < -x) {
+                        mBody->SetLinearVelocity({ -x, vel.y });
+                    }
+                    else if (vel.x > x) {
+                        mBody->SetLinearVelocity({ x, vel.y });
+                    }
+                }
+            }
+
+            if (y) {
+                if (y > 0) {
+                    if (vel.y < -y) {
+                        mBody->SetLinearVelocity({ vel.x, -y });
+                    }
+                    else if (vel.y > y) {
+                        mBody->SetLinearVelocity({ vel.x, y });
+                    }
+                }
+            }
+        }
+    }
 } // namespace Plutus
--- Plutus/src/ECS/Components/RigidBodyComponent.h
+++ Plutus/src/ECS/Components/RigidBodyComponent.h
@@ -13,9 +13,9 @@ class b2Body;
 namespace Plutus
 {
     enum BodyType {
-        StaticBody = b2_staticBody,
-        DynamicBody = b2_dynamicBody,
-        KinematicBody = b2_kinematicBody
+        StaticBody,
+        KinematicBody,
+        DynamicBody
     };
 
     struct Fixture {
@@ -45,15 +45,21 @@ namespace Plutus
 
         Entity mEnt;
         vec2f mOffset;
+        vec2f mMaxVel;
         b2Body* mBody;
         BodyType mBodyType = DynamicBody;
         std::vector<Fixture> mFixtures;
 
-    public:
         RigidBodyComponent() = default;
         RigidBodyComponent(Entity ent, BodyType type, const vec2f& _offset = { 0,0 }, float liDamping = 0, float gScale = 1, float isFixedRotation = true, bool isBullet = false);
 
         void init();
+
+        inline Fixture& addFixture(int type) {
+            mFixtures.push_back({ type });
+            return mFixtures.front();
+        };
+
         void addEdge(vec2f offset, vec2f size, float friction = 0.3f, float density = 1.0f, float restitution = 0, bool isSensor = false);
         void addCircle(vec2f offset, float radius, float friction = 0.3f, float density = 1.0f, float restitution = 0, bool isSensor = false);
         void addBox(vec2f offset, vec2f size, float friction = 0.3f, float density = 1.0f, float restitution = 0, bool isSensor = false);
@@ -61,6 +67,10 @@ namespace Plutus
         void ApplyForce(float x, float y);
         void ApplyImpulse(float x, float y);
         void setVelocity(float x, float y);
+        void speedLimit();
+        inline void setMaxVel(float x, float y) { mMaxVel = { x,y }; }
+
+        vec2f getVelocity();
 
     };
 } // namespace Plutus
