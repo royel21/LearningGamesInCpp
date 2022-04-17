3c6606b4b2f9f2b45a88adcbfa845fe9fa7328b3
royel <royelconsoro@hotmail.com>
2022-04-16 23:27:55 -0400

    testing miniaudio

--- Projects/SingleTest.cpp
+++ Projects/SingleTest.cpp
@@ -1,14 +1,44 @@
+#include <miniaudio.h>
 
-#include <ctime>
-#include <iostream>
-// #include <iomanip>
+#include <stdio.h>
 
-// demonstration of use
-int main() {
+#include <Time/Timer.h>
+#include <Log/Logger.h>
 
-    // std::time_t t = std::time(nullptr);
-    // std::cerr
-    //     << std::put_time(std::localtime(&t), "%T")
-    //     << std::endl;
+int main(int argc, char** argv)
+{
+    ma_result result;
+    ma_engine engine;
+    ma_sound sound;
+
+    result = ma_engine_init(NULL, &engine);
+    if (result != MA_SUCCESS) {
+        printf("Failed to initialize audio engine.");
+        return -1;
+    }
+
+    auto start = Plutus::Timer::micros();
+    result = ma_sound_init_from_file(&engine, "Chunky.mp3", 0, NULL, NULL, &sound);
+
+    ma_sound_start(&sound);
+    ma_sound_set_looping(&sound, true);
+
+
+
+    if (result != MA_SUCCESS) {
+        printf("Failed to initialize audio engine.");
+        return -1;
+    }
+
+    // result = ma_engine_play_sound(&engine, "./assets/sounds/XYZ.ogg", NULL);
+    // ma_engine_play_sound(&engine, "./Chunky.mp3", NULL);
+    Logger::info("Time: %llu", Plutus::Timer::micros() - start);
+    printf("Press Enter to quit...");
+    getchar();
+    ma_sound_stop(&sound);
+
+    ma_sound_uninit(&sound);
+    ma_engine_uninit(&engine);
+
     return 0;
 }
