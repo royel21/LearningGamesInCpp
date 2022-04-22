b741a7f09f2a119e34ef4f074e87044ee7478ae1
royel21 <royelconsoro@hotmail.com>
2022-04-22 08:37:18 -0400

    check layer type

--- Projects/SingleTest.cpp
+++ Projects/SingleTest.cpp
@@ -26,36 +26,37 @@ void readMapData(const std::string& path, TileMapComponent* tilemap, int width =
             for (size_t layerIndex = 0; layerIndex < layers.Size(); layerIndex++)
             {
                 auto& layer = layers[layerIndex].GetJsonObject();
+                if (strcmp(layer["type"].GetString(), "tilelayer") == 0) {
+                    auto props = layer["properties"].GetArray();
+                    tilemap->addTexture(layerIndex, props[0]["value"].GetString());
 
-                auto props = layer["properties"].GetArray();
-                tilemap->addTexture(layerIndex, props[0]["value"].GetString());
+                    int countX = width;
+                    int countY = height;
 
-                int countX = width;
-                int countY = height;
+                    if (!countX || !countY) {
+                        countX = layer["width"].GetInt();
+                        countY = layer["height"].GetInt();
+                    }
 
-                if (!countX && !countY) {
-                    countX = layer["width"].GetInt();
-                    countY = layer["height"].GetInt();
-                }
+                    size_t size = countX * countY;
+                    auto& data = layer["data"].GetArray();
+                    auto dsize = data.Size();
+                    if (size > dsize) {
+                        size = dsize;
+                    }
 
-                size_t size = countX * countY;
-                auto& data = layer["data"].GetArray();
-                auto dsize = data.Size();
-                if (size > dsize) {
-                    size = dsize;
-                }
-
-                for (size_t i = 0; i < size; i++)
-                {
-                    uint32_t d = data[i].GetUint();
-                    if (d) {
-                        int textCoord = 0xfffffff & d;
-                        float rot = 0x1 & (d >> 29) ? 90.0f : 0;
-                        bool flipX = 0x1 & (d >> 30);
-                        bool flipY = 0x1 & (d >> 31);
-                        int x = (i % countX);
-                        int y = (countY - 1) - (i / countY);
-                        tilemap->addTile(Tile{ x,y, textCoord, (uint32_t)layerIndex, flipX, flipY, rot });
+                    for (size_t i = 0; i < size; i++)
+                    {
+                        uint32_t d = data[i].GetUint();
+                        if (d) {
+                            int textCoord = 0xfffffff & d;
+                            float rot = 0x1 & (d >> 29) ? 90.0f : 0;
+                            bool flipX = 0x1 & (d >> 30);
+                            bool flipY = 0x1 & (d >> 31);
+                            int x = (i % countX);
+                            int y = (countY - 1) - (i / countY);
+                            tilemap->addTile(Tile{ x,y, textCoord, (uint32_t)layerIndex, flipX, flipY, rot });
+                        }
                     }
                 }
             }
@@ -67,7 +68,7 @@ int main(int argc, char** argv)
 {
     TileMapComponent tileMap;
 
-    readMapData("assets/tilemaps/testing.json", &tileMap, 10, 10);
+    readMapData("assets/tilemaps/testing.json", &tileMap);
     printf("size: %i", tileMap.mTiles.size());
 
     return 0;
