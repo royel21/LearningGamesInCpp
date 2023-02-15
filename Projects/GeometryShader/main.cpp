a1808dcd11921a1b646f4e810b5c7322fe8d2fc8
royel <royelconsoro@hotmail.com>
2023-02-14 20:10:58 -0400

    some changes

--- Projects/GeometryShader/main.cpp
+++ Projects/GeometryShader/main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char** argv)
 {
     //add support for c++20
-    std::cout << "Testing: " << std::format("{} {}!", "Hello", "world", "something");
     Plutus::AppGeo app;
     app.Run();
     return 0;
