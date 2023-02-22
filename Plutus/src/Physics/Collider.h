eb8662fe5f37e121b9cf4f58dab324a3b7f6f4f0
royel <royelconsoro@hotmail.com>
2023-02-22 06:38:48 -0400

    more changes

--- Plutus/src/Physics/Collider.h
+++ Plutus/src/Physics/Collider.h
@@ -7,17 +7,95 @@
 namespace Plutus
 {
     namespace Collider {
-        // Circle Collision
+        /**
+         * @brief Circle vs Circle Collision
+         *
+         * @param circle1 Circle Shape
+         * @param circle2  Circle Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Circle2d* circle1, Circle2d* circle2, MTV* mtv = nullptr);
+        /**
+        * @brief Circle vs Line Collision
+        *
+        * @param circle1 Circle Shape
+        * @param line  Line Shape
+        * @param mtv Min Translation Vector for SAT separation Struct Pointer
+        * @return true
+        * @return false
+        */
         bool isColliding(Circle2d* circle, Line2d* line, MTV* mtv = nullptr);
+        /**
+         * @brief Circle vs Line Collision
+         *
+         * @param circle1 Circle Shape
+         * @param Box  Line Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Circle2d* circle, Box2d* box, MTV* mtv = nullptr);
-        //Line Collision
+        /**
+         * @brief Line vs Line Collision
+         *
+         * @param line Line Shape
+         * @param line Line Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Line2d* line1, Line2d* line2, MTV* mtv = nullptr);
+        /**
+         * @brief Line vs Circle Collision
+         *
+         * @param line Line Shape
+         * @param circle Circle Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Line2d* line, Circle2d* circle, MTV* mtv = nullptr);
+        /**
+         * @brief Line vs Circle Collision
+         *
+         * @param line Line Shape
+         * @param box Box Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Line2d* line, Box2d* box, MTV* mtv = nullptr);
-        //Box Collision
+        /**
+        * @brief Box vs Box Collision
+        *
+        * @param box Box Shape
+        * @param box Box Shape
+        * @param mtv Min Translation Vector for SAT separation Struct Pointer
+        * @return true
+        * @return false
+        */
         bool isColliding(Box2d* box1, Box2d* box2, MTV* mtv = nullptr);
+        /**
+         * @brief Box vs Circle Collision
+         *
+         * @param box Box Shape
+         * @param circle Circle Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Box2d* box, Circle2d* circle, MTV* mtv = nullptr);
+        /**
+         * @brief Box vs Line Collision
+         *
+         * @param box Box Shape
+         * @param line Line Shape
+         * @param mtv Min Translation Vector for SAT separation Struct Pointer
+         * @return true
+         * @return false
+         */
         bool isColliding(Box2d* box, Line2d* line, MTV* mtv = nullptr);
     }
 } // namespace Plutus
